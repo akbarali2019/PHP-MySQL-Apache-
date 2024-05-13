f18fe86d2524a4d4af62d466cf9cdf8476701c07
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-05-13 13:54:21 +0900

    firstPHPfile commits

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+    //echo 'Hello, Kefa';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title>First PHP File</title>
+    </head>
+
+    <body>
+        <h1><?php echo 'Hello, Kefa'; ?></h1>
+    </body> 
+</html>
