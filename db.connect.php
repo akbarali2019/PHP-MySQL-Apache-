10b0a601ad58c8dca206c3f6b7079f9b966982eb
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-05-15 11:56:19 +0900

    delete db_connect.php commits'

--- db.connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //connect database
-    $conn = mysqli_connect('localhost', '***', '****', 'kefa_pizza');
-
-    //check connection
-    if(!$conn){
-        echo 'Connection Error: ' . mysqli_connect_error();
-    }
-?>
