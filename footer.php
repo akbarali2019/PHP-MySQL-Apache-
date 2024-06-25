79b41da8d367c36974f35b7f7b8fdf481b6e3537
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-06-25 19:00:38 +0900

    footer erp commits

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 
 <footer class="section">
     <div class="center grey-text">Copyright 2024 Kefa Knexus Inc, Ltd </div>
+    <div class="center grey-text">Copyright 2024 KNEXUS-ERP, Ltd </div>
 </footer>
 </body>
